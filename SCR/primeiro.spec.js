d2694c08be9d15ffd7be6c41e4ab5a86719dabd0
Juzenio <juzenio.santos01@gmail.com>
2022-01-13 14:52:29 -0300

    primeito commit

---
+++ SCR/primeiro.spec.js
@@ -0,0 +1,9 @@
+/*primeiro teste*/
+const calculos = require('./pteste.js') 
+
+describe('teste', () => {
+  test('primeiro script erradp', () => {
+
+    expect(calculos.soma(1,2)).toBe(3)
+  })})
+
---
+++ SCR/pteste.js
@@ -0,0 +1,38 @@
+
+
+function chamar() {
+
+
+  
+
+}
+
+function sum() {
+    return a ** b
+    
+  }
+
+  function soma(a, b) {
+    
+    return a + b
+    
+  }
+
+  function div(a, b) {
+    return a - b *5
+  }
+
+  function teste222(element1, element2) {
+    console.log(element1, element2)
+
+    return {
+      element1,
+      element2
+    }
+  }
+
+  console.log(teste222())
+  module.exports = {sum, soma, div}
+  
+  
+ 
