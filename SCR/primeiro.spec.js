d89674a2177d3d54113f8e54ee40770a40bfcb1a
Juzenio <juzenio.santos01@gmail.com>
2022-02-09 17:29:37 -0300

    atualizaçao

--- SCR/primeiro.spec.js
+++
@@ -1,9 +0,0 @@
-/*primeiro teste*/
-const calculos = require('./pteste.js') 
-
-describe('teste', () => {
-  test('primeiro script erradp', () => {
-
-    expect(calculos.soma(1,2)).toBe(3)
-  })})
-
